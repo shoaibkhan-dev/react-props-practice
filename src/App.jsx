4bbde5cd2c290b011cb58dffa201e2228f77e5f0
Shoaib khan <shoaibmangal@yahoo.com>
2025-09-26 23:47:42 +0500

    props practice

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './Navbar/Navbar'
+import Footer from './Footer/Footer'
+import Card from './Card/Card'
+
+function App(){
+  return(
+    <>
+     <Navbar />
+     <Footer />
+     <div className='cards'>
+     <Card title="this is card 1" descreption="card 1 descreption"/>
+     <Card title="this is card 2" descreption="card 2 descreption"/>
+     <Card title="this is card 3" descreption="card 3 descreption"/>
+     <Card title="this is card 4" descreption="card 4 descreption"/>
+     </div>
+    </>
+  )
+}
+export default App
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+   <div class="footer">Shoaib 2025 | All right reserved</div>
+  )
+}
+
+export default Footer
