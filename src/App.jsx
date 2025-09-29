209f5cd462b6f6c75c3e24e84e2d15c498f89fe8
Shoaib khan <shoaibmangal@yahoo.com>
2025-09-29 23:15:40 +0500

    counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,36 @@ import './App.css'
 import Navbar from './Navbar/Navbar'
 import Footer from './Footer/Footer'
 import Card from './Card/Card'
+import { useState } from 'react'
 
-function App(){
+
+function Counter(){
+  const [count, setCount] = useState(0);
+   
+  const resetBtn = () =>{
+    setCount(0);
+  }
+
   return(
-    <>
-     <Navbar />
-     <Footer />
-     <div className='cards'>
-     <Card name="fried chicken" details="high quality fried chicken" price="5.99$"/>
-     <Card name="french fries" details="high quality potato fries"/>
-     <Card name="fried chicken" details="high quality fried chicken"/>
-     <Card name="fried chicken" details="high quality fried chicken" button="add to cart"/>
-     </div>
-    </>
+    <div>
+      <h1>counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
+    
+      <button onClick={resetBtn}>reset</button>
+    </div>
   )
+
+    // <>
+    //  <Navbar />
+    //  <Footer />
+    //  <div className='cards'>
+    //  <Card name="fried chicken" details="high quality fried chicken" price="5.99$"/>
+    //  <Card name="french fries" details="high quality potato fries"/>
+    //  <Card name="fried chicken" details="high quality fried chicken"/>
+    //  <Card name="fried chicken" details="high quality fried chicken" button="add to cart"/>
+    //  </div>
+    // </>
+  
 }
-export default App;
+export default Counter;
